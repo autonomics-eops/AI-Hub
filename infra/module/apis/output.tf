effcb78af3c279ab48645c4ebf818916928ecd1f
autonomics-eops <swati-soni@hcltech.com>
2025-11-11 11:47:09 +0530

    commit 6

--- infra/module/apis/output.tf
+++
@@ -1,4 +0,0 @@
-output "api_name" {
-  description = "API Management API Resource Name"
-  value       = azurerm_api_management_api.api.name
-}
--- infra/module/openai/output.tf
+++
@@ -1,4 +0,0 @@
-output "apim_fqdn" {
-  description = "API Management Service HTTPs Endpoint"
-  value       = azurerm_cognitive_account.this.endpoint
-}

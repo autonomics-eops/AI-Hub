5742303a17c276a4c6d71b464de4e7a5d4a2e17a
autonomics-eops <AUTONOMICS-DEVOPS@hcltech.com>
2025-11-25 17:47:00 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -17,12 +17,12 @@ terraform {
     storage_account_name = "sttfscf1f29544prdwe001"
     container_name       = "tfstate"
     key                  = "aihublz.tfstate"
-    resource_group_name  = "rg-tfstate-prd-we-001"
-    subscription_id      = "c6cb483d-5684-4a38-9d2f-9afb65dc672f"
+    resource_group_name  = "AI-hub"
+    subscription_id      = "4859ec56-96d2-47b5-ae52-3b74174759e1"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "c6cb483d-5684-4a38-9d2f-9afb65dc672f"
+  subscription_id = "4859ec56-96d2-47b5-ae52-3b74174759e1"
 }
